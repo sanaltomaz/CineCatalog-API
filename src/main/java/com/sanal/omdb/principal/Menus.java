19f696654d6125985cb76413dfb4ad6f6c5d75a9
Sanal T R da Silva <tomazsanal@gmail.com>
2025-12-20 16:17:46 -0300

    Implementando funcionalidades dos menus.

--- src/main/java/com/sanal/omdb/principal/Menus.java
+++ src/main/java/com/sanal/omdb/principal/Menus.java
@@ -2,6 +2,8 @@ package com.sanal.omdb.principal;
 
 import java.util.Scanner;
 
+import com.sanal.omdb.models.*;
+
 public class Menus {
     Scanner scanner = new Scanner(System.in);
 
@@ -12,8 +14,17 @@ public class Menus {
         System.out.print("Escolha uma opção: ");
     }
 
-
-    public void funcoesMenu() {
-        // Implementar funcionalidades do menu aqui
+    public void funcoesMenu(Class<?> tipoClass) {
+        if (tipoClass == DadosSerie.class) {
+            System.out.println("Funções disponíveis para séries:");
+            System.out.println("1. Listar episódios");
+            System.out.println("2. Buscar episódio por número");
+            System.out.println("Em criação...");
+            System.out.println("3. Buscar episódios por temporada");
+            System.out.println("4. Exibir melhores episódios");
+            System.out.println("5. Exibir piores episódios");
+            System.out.println("6. Voltar ao menu principal");
+            System.out.print("Escolha uma opção: ");
+        }
     }
 }
